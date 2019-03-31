63ce1ec75ed591d320afd0eb3619382d833fab97
yymin1022 <yymin1022@gmail.com>
2019-03-31 13:26:30 +0900

    Rename File Name Error : Detial to Detail

--- app/src/main/java/com/defcon/electric_car/ui/main/charger_detail/ChargerDetialActivity.kt
+++ app/src/main/java/com/defcon/electric_car/ui/main/charger_detail/ChargerDetialActivity.kt
@@ -1,18 +1,15 @@
 package com.defcon.electric_car.ui.main.charger_detail
 
-import android.content.Context
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.defcon.electric_car.R
-import com.defcon.electric_car.model.ChargerModel
 
 class ChargerDetialActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_charger_detial)
+        setContentView(R.layout.activity_charger_detail)
 
     }
 }
